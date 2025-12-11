251e1f8a0afa4c3052c5668f5ecf153f60e09daa
Ron Hagani <ronhagani55@gmail.com>
2025-12-11 22:59:10 +0200

    updated app.py and added routes

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+tasks= [
+    {"id": 1, "title": "Task 1", "done": False},
+    {"id": 1, "title": "Task 2", "done": False}
+]
+
 @app.route('/')
-def hello():
-    return '{"message": "Task Manager is running!", "status": "success"}'
+def home():
+    return jsonify({"message": "Welcome to Task Manager API"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({"tasks": tasks})
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    new_task = request.get_json()
+    tasks.append(new_task)
+    return jsonify({"message": "Task added!", "task": new_task}), 201
+
+@app.route('/tasks', methods=['DELETE'])
+def delete_all():
+    tasks.clear()
+    return jsonify({"message": "All tasks deleted!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
