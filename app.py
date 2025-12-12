0f183a2dcdfebe836ff0c218c05db91b7ef6edef
Ron Hagani <ronhagani55@gmail.com>
2025-12-12 16:54:06 +0200

    Feat: Add Edit product functionality and PUT endpoint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import time
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
@@ -16,7 +17,8 @@ def get_engine_with_retry():
     while retries > 0:
         try:
             engine = create_engine(DATABASE_URL)
-            engine.connect()
+            with engine.connect():
+                pass
             return engine
         except Exception:
             print(f"Database not ready... retrying in 5s ({retries} left)")
@@ -72,14 +74,50 @@ def get_products():
 @app.post("/products")
 def create_product(product: ProductCreate):
     db = SessionLocal()
-    new_product = ProductModel(
-        name=product.name,
-        description=product.description,
-        price=product.price,
-        stock=product.stock
-    )
-    db.add(new_product)
+
+    existing_product = db.query(ProductModel).filter(ProductModel.name == product.name).first()
+
+    if existing_product:
+        print(f"Product {product.name} exists! Updating stock.")
+        existing_product.stock += product.stock
+        existing_product.price = product.price
+        existing_product.description = product.description
+
+        db.commit()
+        db.refresh(existing_product)
+        db.close()
+        return existing_product
+    
+    else:
+        print(f"New product {product.name}. Creating.")
+        new_product = ProductModel(
+            name = product.name,
+            description = product.description,
+            price = product.price,
+            stock = product.stock
+        )
+
+        db.add(new_product)
+        db.commit()
+        db.refresh(new_product)
+        db.close()
+        return new_product
+
+@app.put('/products/{product_id}')
+def update_product(product_id: int, product: ProductCreate):
+    db = SessionLocal()
+    db_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+    if db_product is None:
+        db.close()
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    db_product.name =  product.name
+    db_product.description = product.description
+    db_product.price = product.price
+    db_product.stock = product.stock
+
     db.commit()
-    db.refresh(new_product)
+    db.refresh(db_product)
     db.close()
-    return new_product
+    return db_product
